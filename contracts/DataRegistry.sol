6c8a30ba7007784b2e2968d3e3035830987da6f3
sorry52 <yk_katirci@hotmail.com>
2025-11-26 23:53:05 +0100

    Handled two files: DataRegistry and DataIdentityRegistry

--- contracts/DataRegistry.sol
+++ contracts/DataRegistry.sol
@@ -3,24 +3,81 @@ pragma solidity ^0.8.28;
 
 contract DataRegistry {
 
+    // dataType codes:
+    // 0 = Ignore (array index convention) 
+    // 1 = LAB_RESULTS
+    // 2 = IMAGING (X-ray, MRI pictures of the inside body
+    // 3 = FULL_RECORD
+
     struct DataPointer {
-        bytes32 pointerHash;
-        uint256 timestamp;
+        bytes32 dataHash;   // keccak256(encrypted off-chain record)
+        uint256 timestamp;  // when this version was registered
+        uint32 version;     // 0, 1, 2..., per (patient, dataType)
     }
 
-    mapping(address => mapping(uint8 => DataPointer)) public dataPointers;
+    // address of the patient is given -> see which type is requested -> point to the array that stored that type of information
+    mapping(address => mapping(uint8 => DataPointer[])) public dataPointers;
 
-    event DataPointerUpdated(address patient, uint8 dataType, bytes32 pointerHash);
+    event DataPointerUpdated(address indexed patient, uint8 indexed dataType, uint32 version, bytes32 dataHash);
 
-    function setDataPointer(uint8 dataType, bytes32 pointerHash) external {
-        dataPointers[msg.sender][dataType] = DataPointer(pointerHash, block.timestamp);
-        emit DataPointerUpdated(msg.sender, dataType, pointerHash);
+    
+    // dataType Encoded type of medical data (For an example : "1" means lab result)
+    // dataHash keccak256(encrypted JSON or binary medical record) for an authorized contract) 
+    
+    // Some examples for better understanding. "Ahmet" is just a name
+    // Ahmet -> lab1 results -> list [] (Contains Data pointer struct elements)
+    // list[0] = data hash : datahash, timestamp: 1534234, version : 1
+    // Ahmet -> lab1 results -> list [] (Contains Data pointer struct elements)
+    // list[1] = data hash : datahash, timestamp: 1534234, version : 2
+
+    function setDataPointer(uint8 dataType, bytes32 dataHash) external {
+        require(dataType <= 3, "Invalid dataType");
+        require(dataHash != bytes32(0), "Empty hash");
+
+        DataPointer[] storage list = dataPointers[msg.sender][dataType];
+        uint32 newVersion = uint32(list.length + 1);
+
+        list.push(
+            DataPointer({
+                dataHash: dataHash,
+                timestamp: block.timestamp,
+                version: newVersion
+            })
+        );
+
+        emit DataPointerUpdated(msg.sender, dataType, newVersion, dataHash);
     }
 
-    function getLatestDataPointer(address patient, uint8 dataType) 
-        external view returns (bytes32, uint256) 
-    {
-        DataPointer memory dp = dataPointers[patient][dataType];
-        return (dp.pointerHash, dp.timestamp);
+    //Get the latest record hash for a patient and dataType
+    function getLatestDataPointer(
+        address patient,
+        uint8 dataType
+    ) external view returns (bytes32 dataHash, uint256 timestamp, uint32 version) {
+        DataPointer[] storage list = dataPointers[patient][dataType];
+        require(list.length > 0, "No data for this type");
+
+        DataPointer storage dp = list[list.length - 1];
+        return (dp.dataHash, dp.timestamp, dp.version);
+    }
+
+    //Get a specific historical version
+    function getDataPointerVersion(
+        address patient,
+        uint8 dataType,
+        uint32 version
+    ) external view returns (bytes32 dataHash, uint256 timestamp) {
+        DataPointer[] storage list = dataPointers[patient][dataType];
+        require(version >= 1 && version <= list.length, "Invalid version");
+
+        DataPointer storage dp = list[version - 1];
+        return (dp.dataHash, dp.timestamp);
+    }
+
+    //To check how many versions exist for this patient+dataType
+    function getVersionCount(
+        address patient,
+        uint8 dataType
+    ) external view returns (uint256) {
+        return dataPointers[patient][dataType].length;
     }
 }
--- contracts/DigitalIdentityRegistry.sol
+++ contracts/DigitalIdentityRegistry.sol
@@ -2,31 +2,188 @@
 pragma solidity ^0.8.28;
 
 contract DigitalIdentityRegistry {
+    // High-level roles in the system
+    enum Role { NONE, PATIENT, DOCTOR, RESEARCHER, INSURANCE, SPECIALIST }
 
-    enum Role { NONE, PATIENT, DOCTOR, RESEARCHER, INSURANCE }
+    uint256 private internalIdCounter;
+    
 
     struct User {
+        uint256 internalId;
         address userAddress;
         Role role;
-        bytes32 hashID;
+        bytes32 hashId;     // Hashed identifier, keccak256(email + medicalId for patients and licenseId or workEmail for staff )
         bool isRegistered;
+        // All other sensitive attributes (name, surname, age, description) are handled off-chain
     }
+
+    address public owner;                // System / hospital admin, head doctor or someone who literally owns the hospital
+    address public instituteResearcher;  // research institute admin (We assume this guy is chosen by the owner)
+    address public doctor; 
 
     mapping(address => User) public users;
+    mapping(uint256 => address) public internalIdToAddress;
 
-    event UserRegistered(address user, Role role);
 
-    function registerPatient(bytes32 _hashID) external {
-        users[msg.sender] = User(msg.sender, Role.PATIENT, _hashID, true);
-        emit UserRegistered(msg.sender, Role.PATIENT);
+    event UserRegistered(address indexed user, Role role, bytes32 hashId);
+    event UserAddressUpdated(uint256 internalId, address oldAddress, address newAddress);
+
+    constructor(address _instituteResearcher, address _doctor) {
+        owner = msg.sender;                     // Deployer is system owner
+        instituteResearcher = _instituteResearcher;
+        doctor = _doctor;
     }
 
-    function registerDoctor(bytes32 _hashID) external {
-        users[msg.sender] = User(msg.sender, Role.DOCTOR, _hashID, true);
-        emit UserRegistered(msg.sender, Role.DOCTOR);
+    //Modifiers start
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
     }
 
-    function getUser(address _addr) external view returns (User memory) {
-        return users[_addr];
+    modifier onlyOwnerOrInstitute() {
+        require(msg.sender == owner || msg.sender == instituteResearcher, "Only owner or institute");
+        _;
     }
+
+    modifier onlyDoctor(){
+        require (msg.sender == doctor, "Violating hippocratic oath is not allowed");
+        _;
+    }
+    //Modifiers end
+
+    // To register people
+    function _register(
+        address account,
+        Role role,
+        bytes32 hashId
+    ) internal {
+        require(!users[account].isRegistered, "User already registered");
+        internalIdCounter += 1;
+        uint256 newId = internalIdCounter;
+
+        users[account] = User({
+            internalId: newId,
+            userAddress: account,
+            role: role,
+            hashId: hashId,
+            isRegistered: true
+        });
+
+        internalIdToAddress[newId] = account;
+
+        emit UserRegistered(account, role, hashId);
+    }
+
+    // Patients register themselves (This can be done through a small kiosk with a good ui interface in hospitals in real life)
+    // hashId = web3.utils.keccak256(email + medicalId) done off-chain(the chain will only see the hash but off chain we should be able to see email + medicalId)
+    function registerPatient(
+        bytes32 hashId
+        // name, surname, age, shortDescription are handled off-chain and NOT sent on-chain for privacy
+    ) external {
+        _register(msg.sender, Role.PATIENT, hashId);
+    }
+
+    // Owner registers doctors (e.g. hospital admin, head doctor or someone who literally owns the hospital)
+    // doctorAddress: Ethereum address of the doctor
+    // hashId: web3.utils.keccak256(licenseId or workEmail)
+    function registerDoctor(
+        address doctorAddress,
+        bytes32 hashId
+        // name, surname, age are handled off-chain
+    ) external onlyOwner {
+        _register(doctorAddress, Role.DOCTOR, hashId);
+    }
+
+    // Owner or research institute registers researchers
+    // researcherAddress: Ethereum address of the researcher
+    function registerResearcher(
+        address researcherAddress,
+        bytes32 hashId
+        // name, surname, age are handled off-chain
+    ) external onlyOwnerOrInstitute {
+        _register(researcherAddress, Role.RESEARCHER, hashId);
+    }
+
+    // Owner registers insurance employees
+    // employeeAddress: Ethereum address of the insurance employee
+    function registerInsurance(
+        address employeeAddress,
+        bytes32 hashId
+        // name, surname, age are handled off-chain
+    ) external onlyOwner {
+        _register(employeeAddress, Role.INSURANCE, hashId);
+    }
+
+    //To make it more readable the desired result
+    function getPatient(address account) external onlyDoctor view returns (
+        address userAddress,
+        Role role,
+        bytes32 hashId,
+        bool isRegistered,
+        uint256 internalId
+    )  {
+        User memory u = users[account];
+        require(u.role == Role.PATIENT, "This is only for looking patients");
+
+        return (
+            u.userAddress,
+            u.role,
+            u.hashId,
+            u.isRegistered,
+            u.internalId // For off chain hashing / linking to medical record
+        );
+    }
+
+    //To make it more readable the desired result (Allow only doctors to make sure that the hippocratic oath is not violated)
+    function getStaff(address account) external onlyOwner view returns (
+        address userAddress,
+        Role role,
+        bytes32 hashId,
+        bool isRegistered,
+        uint256 internalId
+    ){
+        User memory u = users[account];
+        require(u.role != Role.PATIENT, "What you do is illegal!");
+
+        return (
+            u.userAddress,
+            u.role,
+            u.hashId,
+            u.isRegistered,
+            u.internalId
+        );
+    }
+
+    function updateUserAddress(uint256 internalId, address newAddress) external onlyOwner {
+    require(newAddress != address(0), "Invalid new address");
+    
+    // Find current address for this internalId
+    address currentAddress = internalIdToAddress[internalId];
+    require(currentAddress != address(0), "Unknown internalId");
+
+    // Make sure new address is not already registered
+    require(!users[newAddress].isRegistered, "New address already registered");
+
+    // Load existing user
+    User storage u = users[currentAddress];
+
+    // Recreate user under new address
+    users[newAddress] = User({
+        internalId: u.internalId,
+        userAddress: newAddress,
+        role: u.role,
+        hashId: u.hashId,
+        isRegistered: u.isRegistered
+    });
+
+    // Clean up old mapping entry (This delete keyword resets a variable to its default value, as if it was never set.)
+    delete users[currentAddress];
+
+    // Update reverse index
+    internalIdToAddress[internalId] = newAddress;
+
+    emit UserAddressUpdated(internalId, currentAddress, newAddress);
+}
+
+
 }
